04ee841426f11eca151e20376dd96560c48e0ad4
irafajs <irafajs@gmail.com>
2023-08-08 21:18:25 +0000

    PY T04 HEXA DECI

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for decimal in range(0, 99):
-    hexa = hex(decimal)
-    print(f"{decimal} = {hexa}")
+    hexa = "0x{:X}".format(decimal)
+    print("{:2} = {:4}".format(decimal, hexa))
