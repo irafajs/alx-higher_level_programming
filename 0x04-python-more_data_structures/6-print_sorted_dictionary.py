c20f369ad3aa9bd39fdede19bb0300e5cc56b32e
irafajs <irafajs@gmail.com>
2023-08-16 22:01:50 +0000

    DATA STRUCTURE T06

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if (a_dictionary):
-        for fung, agac in sorted(a_dictionary.items()):
-            print("{} : {}".format(fung, agac))
+    for fung, agac in sorted(a_dictionary.items()):
+        print("{}: {}".format(fung, agac))
