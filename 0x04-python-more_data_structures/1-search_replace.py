3bdec7fe31f3b161443fc30a01c94fcb4a03df40
irafajs <irafajs@gmail.com>
2023-08-16 19:41:27 +0000

    DATA STRUC T02 PY

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if search < 0 or search >= len(my_list):
+    if search < 0 or search >= len(my_list) or replace == None:
         return my_list.copy()
     new_l = my_list.copy()
     new_l[search - 1] = replace
