eb66ab16a9a8eb327a99b2327492876a2f4db007
irafajs <irafajs@gmail.com>
2023-08-16 19:28:52 +0000

    DATA STRUCTURE SET DICT TASK01

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if search < 0 or search >= len(my_list):
+        return my_list.copy()
+    new_l = my_list.copy()
+    new_l[search - 1] = replace
+    return new_l
