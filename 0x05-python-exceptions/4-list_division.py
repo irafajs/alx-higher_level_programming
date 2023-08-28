de065767fb6408aa8e8456d8382c9c5ffae362a5
irafajs <irafajs@gmail.com>
2023-08-28 22:23:55 +0000

    CORRECTION

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,30 +1,27 @@
 def list_division(my_list_1, my_list_2, list_length):
-    results = []
-    error_messages = []
-    try:
-        for i in range(list_length):
+    result = []
+    if len(my_list_1) < list_length or len(my_list_2) < list_length:
+        print("out of range")
+    for i in range(list_length):
+        try:
             elem1 = my_list_1[i] if i < len(my_list_1) else 0
-            elem2 = my_list_2[i] if i < len(my_list_2) else 1
-            c_int_el1 = isinstance(elem1, (int, float))
-            c_int_el2 = isinstance(elem2, (int, float))
-            if not (c_int_el1 and c_int_el2):
-                error_messages.append("wrong type")
-                results.append(0)
-            else:
-                try:
-                    division_result = elem1 / elem2
-                    if division_result == 0:
-                        results.append(0)
-                    else:
-                        results.append(division_result)
-                except ZeroDivisionError:
-                    error_messages.append("division by 0")
-                    results.append(0)
-    except IndexError:
-        pass
-    finally:
-        if len(my_list_1) < list_length or len(my_list_2) < list_length:
-            error_messages.append("out of range")
-        for error in error_messages:
-            print(error)
-        return results
+            elem2 = my_list_2[i] if i < len(my_list_2) else 0
+            c_i_elem1 = isinstance(elem1, (int, float))
+            c_i_elem2 = isinstance(elem2, (int, float))
+            if not c_i_elem1 or not c_i_elem2:
+                raise TypeError("wrong type")
+            if elem2 == 0:
+                raise ZeroDivisionError("division by 0")
+            result.append((elem1 / elem2))
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+        finally:
+            pass
+    return result
