420c672ec454499a0a4da8648ad20cc88b2b7cbd
irafajs <irafajs@gmail.com>
2023-08-29 22:27:54 +0000

    PY CLASSES T04

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""defines class named Square"""
+
+
+class Square:
+    """defines a function called __init__"""
+    def __init__(self, size=0):
+        """initialize size to size"""
+        self.size = size
+
+    """defines a function called size"""
+    @property
+    def size(self):
+        """returns size"""
+        return self.__size
+    """define a function called size with 2 objects"""
+    @size.setter
+    def size(self, value):
+        """if statement"""
+        if not isinstance(value, int):
+            """raise an error"""
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            """raise an error"""
+            raise ValueError("size must be >= 0")
+        else:
+            """initialize __size with value"""
+            self.__size = value
+    """defines a public function called area"""
+    def area(self):
+        """return power of 2 of __size"""
+        return self.__size ** 2
