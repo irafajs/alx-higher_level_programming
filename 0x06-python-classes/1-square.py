3fbe4d1b9373638217fe13293f658ea5fd840111
irafajs <irafajs@gmail.com>
2023-08-29 21:11:33 +0000

    PY CLASSESS T01

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""define class named Square"""
+
+
+class Square:
+    """define a method called __init__"""
+    def __init__(self, size):
+        """set size to privat and initialize it"""
+        self.i__size = size
