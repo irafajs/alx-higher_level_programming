0fa51cfff1a6f731921f389ab5fe2850fa05e79c
irafajs <irafajs@gmail.com>
2023-09-05 12:41:33 +0000

    PY TEST DRIVEN DEV T00

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """addidtion function"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        a = int(a)
+        b = int(b)
+        return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,10 @@
+THIS DIRECTROY CONTAINS EXERCISES ABOUT TEST DRIVEN 
+		DEVELOPMENT
+___________________________________________________
+
+0. Integers addition
+1. Divide a matrix
+2. Say my name
+3. Print square
+4. Text indentation
+5. Max integer - Unittest
