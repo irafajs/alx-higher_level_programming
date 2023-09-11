6d5105b77d1e3073a31b6211e96bf8226f7fd4d0
irafajs <irafajs@gmail.com>
2023-09-11 14:21:22 +0000

    PY INHERITANCE T00

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+this is a shebang always start when you create a python script
+"""
+
+
+def lookup(obj):
+    """return available att and methofs of an object"""
+    return dir(obj)
