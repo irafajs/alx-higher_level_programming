bef6e58a51f67a02f776512567e1e9a07a564b0a
irafajs <irafajs@gmail.com>
2023-09-11 20:38:34 +0000

    PY INHERITANCE T02

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This is A shebang, has to be first line of pythonr script
+"""
+
+
+def is_same_class(obj, a_class):
+    """true if object is instance of class else false"""
+    return obj.__class__ == a_class
