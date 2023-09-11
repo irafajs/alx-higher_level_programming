998eddce06daaf4cf1407a27df96e1537d476dfa
irafajs <irafajs@gmail.com>
2023-09-11 20:50:15 +0000

    PY INHERITANCE T03

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+this is a SHEBANG used in PYTHON scripting
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """return true is obj is part of a_class else false"""
+    return isinstance(obj, a_class)
