ee91548c4d065e09f5b03709b1d7fa12660b3895
irafajs <irafajs@gmail.com>
2023-09-11 21:28:33 +0000

    PY INHERITANCE T06

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is Shebang used to make PY cripting
+"""
+
+
+class BaseGeometry:
+    """class named BaseGeometry"""
+    def area(self):
+        """method that raise an exception"""
+        raise Exception("area() is not implemented")
