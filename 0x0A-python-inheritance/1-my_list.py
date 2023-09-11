06898abea9e6d28b2925dbcdf95169b7fe51dcd9
irafajs <irafajs@gmail.com>
2023-09-11 19:13:39 +0000

    PY INHERITANCE COR

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,6 +6,10 @@ this is a Shebang used to make python script
 
 class MyList(list):
     """class named my list that inherite list object)"""
+    def __init__(self):
+        """initialize init method"""
+        super().__init__()
+
     def print_sorted(self):
         """method to print sorted list"""
         s_list = sorted(self)
