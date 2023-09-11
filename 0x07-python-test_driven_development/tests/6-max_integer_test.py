d58d69231a14eefa291b89f3b57295827297c83b
irafajs <irafajs@gmail.com>
2023-09-11 09:08:48 +0000

    PY TEST DRIVEN DEV T06

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,13 +4,19 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-class TestMaxInteger(unittest.TestCase):
 
+class TestMaxInteger(unittest.TestCase):
+    """
+    class to test using unittest module
+    """
     def test_max_integer(self):
-        self.assertEqual(max_integer([1, 2, 3,4, -100]), 4)
+        """methods to test max int when passed mixed int"""
+        self.assertEqual(max_integer([1, 2, 3, 4, -100, 10.7]), 10.7)
 
     def test_max_integer_empty(self):
+        """method to test max int when list is empty"""
         self.assertIsNone(max_integer([]))
 
     def test_max_integer_1element(self):
+        """method to test max int when list has one element"""
         self.assertEqual(max_integer([10]), 10)
