4a2f00a26460e47e23dd4d39f505998e69893647
irafajs <irafajs@gmail.com>
2023-09-12 19:55:50 +0000

    PY INPUT OUT T00

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Shebang to create a python script
+"""
+
+
+def read_file(filename=""):
+    """function to read file content"""
+    with open('my_file_0.txt', encoding='utf-8') as my_f:
+        print("{}".format(my_f.read()))
