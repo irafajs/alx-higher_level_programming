b6af9353b5dca4427e499ad4d2562bfa6e15fc13
irafajs <irafajs@gmail.com>
2023-09-12 20:01:12 +0000

    PY INPUT OUT T00 COR

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ Shebang to create a python script
 def read_file(filename=""):
     """function to read file content"""
     with open('my_file_0.txt', encoding='utf-8') as my_f:
-        print("{}".format(my_f.read()))
+        print("{}".format(my_f.read()), end="")
