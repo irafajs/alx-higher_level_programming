ceb0572556bd1674f7dcf0c4c6e18685dca4d0e4
irafajs <irafajs@gmail.com>
2023-09-12 20:51:02 +0000

    PY INPUT OUTPUT T02

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Shebang to create a python script
+"""
+
+
+def append_write(filename="", text=""):
+    """function to append text to the file"""
+    with open(filename, mode='a+', encoding='utf-8') as myfile:
+        myfile.write(text)
+        return myfile.tell()
