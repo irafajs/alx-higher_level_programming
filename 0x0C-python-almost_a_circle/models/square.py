c5a95e4d571c90ce9b56e92c5ea4fc57d277d195
irafajs <irafajs@gmail.com>
2023-09-18 20:19:03 +0000

    PY ALMOST A CIRCLE T10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,7 +12,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """function to initiate the attributes"""
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     def __str__(self):
         """print out the formatted output"""
