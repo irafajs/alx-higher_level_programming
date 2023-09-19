38c8844739a970252d79c015070d3f62f58516b3
irafajs <irafajs@gmail.com>
2023-09-19 15:41:45 +0000

    NEW TASKS

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@ Shebang to create a python script
 
 
 import json
+import csv
 
 
 class Base:
@@ -54,3 +55,58 @@ class Base:
         inst_dum.__dict__.update({'width': 1, 'height': 1, 'x': 0, 'y': 0})
         inst_dum.update(**dictionary)
         return inst_dum
+
+    @classmethod
+    def load_from_file(cls):
+        """method that returns a list of instance"""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, 'r') as r_file:
+                json_data = r_file.read()
+                obj_dicts = cls.from_json_string(json_data)
+                instances = [cls.create(**obj_dict) for obj_dict in obj_dicts]
+                return instances
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """write to a file format .csv"""
+        filename = cls.__name__ + ".csv"
+        with open(filename, 'w', newline='') as w_file:
+            writer = csv.writer(w_file)
+            for obj in list_objs:
+                if cls.__name__ == 'Rectangle':
+                    writer.writerow(
+                            [obj.id, obj.width, obj.height, obj.x, obj.y])
+                elif cls.__name__ == 'Square':
+                    writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """read from a .csv file format"""
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, 'r', newline='') as r_file:
+                reader = csv.reader(r_file)
+                instances = []
+                for row in reader:
+                    if cls.__name__ == 'Rectangle':
+                        obj_dict = {
+                                'id': int(row[0]),
+                                'width': int(row[1]),
+                                'height': int(row[2]),
+                                'x': int(row[3]),
+                                'y': int(row[4])
+                                }
+                    elif cls.__name__ == 'Square':
+                        obj_dict = {
+                                'id': int(row[0]),
+                                'size': int(row[1]),
+                                'x': int(row[2]),
+                                'y': int(row[3])
+                                }
+                    instances.append(cls.create(**obj_dict))
+                return instances
+        except FileNotFoundError:
+            return []
