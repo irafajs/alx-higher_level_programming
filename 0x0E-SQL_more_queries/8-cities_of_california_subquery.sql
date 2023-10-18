5f2b870defc11ca21567db19bf346cc3763f7a4a
irafajs <irafajs@gmail.com>
2023-10-18 20:44:46 +0200

    T08 CORRECTION

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Script to list all cities of Califonia in a given database
-SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
