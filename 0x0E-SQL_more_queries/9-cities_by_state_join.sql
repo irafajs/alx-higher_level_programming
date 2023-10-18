704d14166125eb8caa392a1413d26127b8dc8cf6
irafajs <irafajs@gmail.com>
2023-10-18 21:48:59 +0200

    T09 CORRECTION

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Script to lists all Cities in a given database
-SELECT cities.id, cities.name, states.name from cities LEFT JOIN states on cities.state_id = states.id ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name from cities LEFT JOIN states on states.id = cities.state_id ORDER BY cities.id;
