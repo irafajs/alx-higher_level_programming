dbd314c7a40df9e1e99fbf311638159705bad542
irafajs <irafajs@gmail.com>
2023-10-18 22:28:20 +0200

    MORE SQL QUERIES T10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,2 @@
+-- Script to list all shows that are a given table and has one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
