c77ca2b8f8675c8c9998098c50e306113f9dd4f3
irafajs <irafajs@gmail.com>
2023-10-19 15:03:22 +0200

    SQL INTRO T17 CORRECTION

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
 -- Script convert given database to UTF8
-USE testDB2;
-ALTER TABLE First_name CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
