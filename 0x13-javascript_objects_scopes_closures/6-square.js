430e3bdc9fd935c0704ee47530d0900c8a55bfd2
irafajs <irafajs@gmail.com>
2023-11-14 23:22:05 +0200

    JS OSC TASK 06-10

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const squareVer = require('./5-square');
+
+class Square extends squareVer {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
