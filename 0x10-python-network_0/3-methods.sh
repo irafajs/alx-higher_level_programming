855e356ccc6a6cc935aa9d5d48eedd099947b53c
irafajs <irafajs@gmail.com>
2023-11-30 21:13:22 +0200

    PY NETWORK TASK03

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #bash to display method that the server accept
-curl -sI -X OPTIONS "$1" | grep "Allow" | awk '{print $2}'
+curl -sI -X OPTIONS "$1" | grep -w "Allow" | awk '{sub(/Allow: /, ""); print}'
