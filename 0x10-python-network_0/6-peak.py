61a8476c61c7ed9b129a3b397a9436a12a40fb90
irafajs <irafajs@gmail.com>
2023-11-30 23:31:41 +0200

    PY N T06 FULLUPDATED

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,6 +9,10 @@ def find_peak(list_of_integers):
     if not listoi:
         return None
     small, big = 0, len(listoi) - 1
+    if len(listoi) == 1:
+        return listoi[0]
+    if len(listoi) == 2:
+        return max(listoi)
     while small < big:
         mid = (small + big) // 2
         if listoi[mid] >= listoi[mid + 1] and listoi[mid] >= listoi[mid - 1]:
