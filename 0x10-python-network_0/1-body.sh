d30fd14bbe71c685bebc8fce35b5626f9659a751
irafajs <irafajs@gmail.com>
2023-11-30 20:32:00 +0200

    PY NETWORK TASK 01

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#bash to count size of the reponse of a request send 
-response=$(curl -s -w "%{http_code}" "$1"); status_code=${response: -3}; [ "$status_code" -eq 200 ] && curl -s "$1" echo " this is :$status_code" || exit
+#bash to count size of the reponse of a request send
+curl -sL "$1"
