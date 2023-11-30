3ae2542dc18eb2fdae574888ce30382d5aba9626
irafajs <irafajs@gmail.com>
2023-11-30 23:27:10 +0200

    PY NETWORK TASK06 FULL

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,13 +5,16 @@ Shebang to make PY script
 
 
 def find_peak(list_of_integers):
-    if not list_of_integers:
+    listoi = list_of_integers
+    if not listoi:
         return None
-    small, big = 0, len(list_of_integers) - 1
+    small, big = 0, len(listoi) - 1
     while small < big:
         mid = (small + big) // 2
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
+        if listoi[mid] >= listoi[mid + 1] and listoi[mid] >= listoi[mid - 1]:
             big = mid
-        else:
+        if listoi[mid] < listoi[mid + 1]:
             small = mid + 1
-    return list_of_integers[small]
+        else:
+            big = mid
+    return listoi[small]
