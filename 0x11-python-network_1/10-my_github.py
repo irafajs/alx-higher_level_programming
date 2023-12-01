623c36b6d421363297639bd1e6ec9b5ab227ebbe
irafajs <irafajs@gmail.com>
2023-12-02 00:19:24 +0200

    PY NET1 TASK10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Shebang to make PY script
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+    
+    if len(sys.argv) != 3:
+        sys.exit(1)
+
+    username = sys.argv[1]
+    key_word = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+
+    response = requests.post(url, auth=(username, key_word))
+    if response.status_code == 200:
+        user_data = response.json()
+        print(user_data['id'])
+    else:
+        print(None)
