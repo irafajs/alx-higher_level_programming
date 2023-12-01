17c76dfc25e880ff5d730447c67d2dd9f2f0281e
irafajs <irafajs@gmail.com>
2023-12-01 16:02:19 +0200

    CORRECTION

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,12 @@ Shebang to make PY script
 """
 
 
-import urllib.request
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as response:
-    content = response.read()
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content.decode('utf-8'))
+if __name__ == '__main__':
+    import urllib.request
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- content:", content.decode('utf-8'))
