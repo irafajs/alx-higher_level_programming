e85405ec092af5ff918340954058166c92c368c6
irafajs <irafajs@gmail.com>
2024-01-03 23:20:07 +0200

    JS WEB SCRAPING TASK00

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+function readAndPrintFileContent(filePath) {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(data);
+        }
+    });
+}
+const filePath = process.argv[2];
+readAndPrintFileContent(filePath);
