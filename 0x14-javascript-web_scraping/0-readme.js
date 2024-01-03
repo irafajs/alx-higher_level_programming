780a9288406f6598088fd6068a1057cc910103d9
irafajs <irafajs@gmail.com>
2024-01-03 13:34:40 -0800

    UPDATE TASK00

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,14 +2,14 @@
 
 const fs = require('fs');
 
-function readAndPrintFileContent(filePath) {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log(data);
-        }
-    });
+function readAndPrintFileContent (filePath) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(data);
+    }
+  });
 }
 const filePath = process.argv[2];
 readAndPrintFileContent(filePath);
