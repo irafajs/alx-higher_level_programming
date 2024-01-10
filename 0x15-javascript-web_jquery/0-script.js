790909889f81acab2f36c4be88533eaf08bb83c1
irafajs <irafajs@gmail.com>
2024-01-10 22:02:50 +0200

    TASK0 JS

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const myParagraph = document.querySelector('header');
+  if (myParagraph) {
+    myParagraph.style.color = '#FF0000';
+  }
+});
